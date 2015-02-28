91a0641850087fd24bd6a914c4de92bb014b4a53
emersonmatsumoto <japinha02@gmail.com>
2015-02-28 12:03:54 -0300

    Inserir Categorias e alteração de IRepository

---
+++ FormularioDinamico.Application.Test/InserirCategoriaTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FormularioDinamico.Domain;
+using System.Threading.Tasks;
+using Moq;
+using FormularioDinamico.Domain.Repositories;
+
+namespace FormularioDinamico.Application.Test
+{
+    [TestClass]
+    public class InserirCategoriaTest
+    {
+        [TestMethod]
+        public async Task InserindoCategoria()
+        {
+            var entity = new Categoria();
+            var repository = new Mock<ICategoriaRepository>();
+            repository
+                .Setup(s => s.Insert(It.IsAny<Categoria>()))
+                .Verifiable();
+            
+            repository
+                .Setup(s => s.SaveAsync())       
+                .Returns(Task.Delay(1))
+                .Verifiable();
+
+            InserirCategoria testClass = new InserirCategoria(repository.Object);
+                        
+            await testClass.Executar(entity);
+
+            repository.Verify(v => v.Insert(entity));
+            repository.Verify(v => v.SaveAsync());
+        }
+    }
+}
--- FormularioDinamico.Domain/Repositories/IRepository.cs
+++ FormularioDinamico.Domain/Repositories/IRepository.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace FormularioDinamico.Domain.Repositories
 {
     public interface IRepository<T>
     {
         void Insert(T entity);
+        void Update(T entity);
         void Delete(T entity);
+        Task SaveAsync();
         IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate);
         IQueryable<T> GetAll();
         T GetById(int id);
