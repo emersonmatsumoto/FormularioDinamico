c8b7f707bc7d3ba9fbebb0475d793e9a3e006888
emersonmatsumoto <japinha02@gmail.com>
2015-03-02 15:38:54 -0800

    AutoMapper

---
+++ FormularioDinamico/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FormularioDinamico.BindModels;
+using FormularioDinamico.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormularioDinamico.App_Start
+{
+    public class AutoMapperConfig
+    {
+        public static void RegisterMap()
+        {
+            Mapper.CreateMap<Categoria, CategoriaBM>();
+            Mapper.CreateMap<SubCategoria, SubCategoriaBM>();
+            Mapper.CreateMap<Campo, CampoBM>();
+            Mapper.CreateMap<Item, ItemBM>();            
+        }
+ 
+    }
+}
