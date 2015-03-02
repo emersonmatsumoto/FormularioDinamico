ebe95eb49ecd6fb16153473182119a76c6e7e751
emersonmatsumoto <japinha02@gmail.com>
2015-03-02 15:47:45 -0800

    Removido Item

--- FormularioDinamico.Domain/Campo.cs
+++ FormularioDinamico.Domain/Campo.cs
@@ -9,14 +9,6 @@ namespace FormularioDinamico.Domain
         public int Ordem { get; set; }
         public string Descricao { get; set; }
         public TipoCampo Tipo { get; set; }
-        public ICollection<Item> Lista { get; set; }
-    }
-
-    public class Item
-    {
-        public virtual Campo Campo { get; set; }
-        public int CampoId { get; set; }
-        public int Id { get; set; }
-        public string Texto { get; set; }
+        public string Lista { get; set; }
     }
 }
